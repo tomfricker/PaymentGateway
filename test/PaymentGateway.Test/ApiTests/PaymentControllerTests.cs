fb7498871b6e0b7a8ce92b2ce0d4e830656ccd23
Tom Fricker <fricker_tom@hotmail.com>
2020-10-02 21:31:47 +0100

    GetPayment Tests

--- test/PaymentGateway.Test/ApiTests/PaymentControllerTests.cs
+++ test/PaymentGateway.Test/ApiTests/PaymentControllerTests.cs
@@ -1,11 +1,81 @@
-﻿using System;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using PaymentGateway.API.Controllers;
+using PaymentGateway.API.Extensions;
+using PaymentGateway.API.Models;
+using PaymentGateway.API.Services.Contracts;
+using PaymentGateway.Data.Models;
+using PaymentGateway.Data.Repositories.Contracts;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace PaymentGateway.Test.ApiTests
 {
     public class PaymentControllerTests
     {
+        [Fact]
+        public async Task Get_NullPayment_ReturnsNotFound()
+        {
+            var mockPaymentRepo = new Mock<IPaymentRepository>();
+            var mockLogger = new Mock<ILogger<PaymentsController>>();
+            var mockMapper = new Mock<IMapper>();
+            var mockBankRequestService = new Mock<IBankRequestService>();
+
+            mockPaymentRepo.Setup(x => x.GetPaymentAsync(It.IsAny<Guid>())).ReturnsAsync(() => null);
+
+            var paymentsController =  new PaymentsController(mockPaymentRepo.Object, mockBankRequestService.Object, mockMapper.Object, mockLogger.Object);
+            
+            var response = await paymentsController.Get(Guid.NewGuid());
+
+            Assert.IsType<NotFoundResult>(response);
+        }
+
+        [Fact]
+        public async Task Get_PaymentReturns_ReturnsOkResult()
+        {
+            var mockPaymentRepo = new Mock<IPaymentRepository>();
+            var mockLogger = new Mock<ILogger<PaymentsController>>();
+            var mockMapper = new Mock<IMapper>();
+            var mockBankRequestService = new Mock<IBankRequestService>();
+
+            var mapperResponse = new GetPaymentResponse
+            {
+                Name = "Fred Bloggs",
+                CardNumber = "4111111111111111",
+                Id = Guid.NewGuid(),
+                PaymentStatus = Core.Enums.PaymentStatusCode.Completed
+            };
+
+            mockPaymentRepo.Setup(x => x.GetPaymentAsync(It.IsAny<Guid>())).ReturnsAsync(() => new Payment());
+            mockMapper.Setup(x => x.Map<GetPaymentResponse>(It.IsAny<Payment>())).Returns(mapperResponse);
+
+            var paymentsController = new PaymentsController(mockPaymentRepo.Object, mockBankRequestService.Object, mockMapper.Object, mockLogger.Object);
+
+            var response = await paymentsController.Get(Guid.NewGuid());
+
+            var okResponse = Assert.IsType<OkObjectResult>(response);
+            var getPaymentResponse = Assert.IsType<GetPaymentResponse>(okResponse.Value);
+            Assert.Equal(mapperResponse.CardNumber.MaskCard(), getPaymentResponse.CardNumber);
+            Assert.Equal(mapperResponse.Name, getPaymentResponse.Name);
+            Assert.Equal(mapperResponse.Id, getPaymentResponse.Id);
+            Assert.Equal(mapperResponse.PaymentStatus, getPaymentResponse.PaymentStatus);
+        }
+
+        private PaymentsController SetupTestController()
+        {
+            var mockPaymentRepo = new Mock<IPaymentRepository>();
+            var mockLogger = new Mock<ILogger<PaymentsController>>();
+            var mockMapper = new Mock<IMapper>();
+            var mockBankRequestService = new Mock<IBankRequestService>();
+
+            mockPaymentRepo.Setup(x => x.GetPaymentAsync(It.IsAny<Guid>())).ReturnsAsync(() => null);
+
+            return new PaymentsController(mockPaymentRepo.Object, mockBankRequestService.Object, mockMapper.Object, mockLogger.Object);
+        }
     }
 }
