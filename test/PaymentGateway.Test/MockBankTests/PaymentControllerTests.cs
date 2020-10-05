7c94e378d617ecfb746c7893cc83c7b81f70e9f7
Tom Fricker <fricker_tom@hotmail.com>
2020-10-05 21:50:49 +0100

    Mock Bank test

---
+++ test/PaymentGateway.Test/MockBankTests/PaymentControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaymentGateway.Core.Models;
+using PaymentGateway.Mock.BankA.Controller;
+using PaymentGateway.Mock.BankA.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PaymentGateway.Test.MockBankTests
+{
+    public class PaymentControllerTests
+    {
+        [Fact]
+        public void Post_ReturnsBankResponse()
+        {
+            var paymentController = new PaymentController();
+
+            var response = paymentController.Post(new BankRequest());
+
+            Assert.IsType<BankResponse>(response);
+            Assert.InRange((int)response.PaymentStatus, 0, 2);
+        }
+    }
+}
